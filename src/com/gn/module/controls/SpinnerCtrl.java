0f74b760b66d13f183b6795aab7d2bb4a3ddcf5f
Gleidson <gleidisonmt@gmail.com>
2018-11-06 16:29:45 -0200

    add spinner factory

---
+++ src/com/gn/module/controls/SpinnerCtrl.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (C) Gleidson Neves da Silveira
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.gn.module.controls;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * @author Gleidson Neves da Silveira | gleidisonmt@gmail.com
+ * Create on  06/11/2018
+ * Version 1.0
+ */
+public class SpinnerCtrl implements Initializable {
+
+    @FXML private Spinner<Integer> spinner;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        SpinnerValueFactory valueFactory = new SpinnerValueFactory.IntegerSpinnerValueFactory(0, 100);
+        spinner.setValueFactory(valueFactory);
+    }
+}
