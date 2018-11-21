08f97e1e28c7c97a29c0cf106bff3aaa5974f33c
Gleidson <gleidisonmt@gmail.com>
2018-11-21 11:57:11 -0200

    add Carousel

---
+++ src/com/gn/module/designer/Carousel.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) Gleidson Neves da Silveira
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.gn.module.designer;
+
+import com.gn.GNCarousel;
+import javafx.animation.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Pagination;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Screen;
+import javafx.util.Callback;
+import javafx.util.Duration;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * @author Gleidson Neves da Silveira | gleidisonmt@gmail.com
+ * Create on  13/10/2018
+ * Version 1.0
+ */
+public class Carousel implements Initializable {
+
+    @FXML private GNCarousel carousel;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        carousel.getItems().setAll(createItems());
+        carousel.setVelocity(Duration.millis(500));
+        carousel.setTitle("Title");
+        carousel.setSubtitle("This is a subtitle");
+        carousel.setCycleCount(5000, 5000);
+    }
+    private ObservableList<Node> createItems(){
+
+        Label lb1 = new Label("First");
+        Label lb2 = new Label("Second");
+        Label lb3 = new Label("Third");
+        Label lb4 = new Label("Fourth");
+        Label lb5 = new Label("Fifth");
+
+        lb1.setStyle("-fx-text-fill : white;");
+        lb2.setStyle("-fx-text-fill : white;");
+        lb3.setStyle("-fx-text-fill : white;");
+        lb4.setStyle("-fx-text-fill : white;");
+        lb5.setStyle("-fx-text-fill : white;");
+
+        VBox v1 = new VBox(lb1);
+        VBox v2 = new VBox(lb2);
+        VBox v3 = new VBox(lb3);
+        VBox v4 = new VBox(lb4);
+        VBox v5 = new VBox(lb5);
+
+        v1.setAlignment(Pos.CENTER);
+        v2.setAlignment(Pos.CENTER);
+        v3.setAlignment(Pos.CENTER);
+        v4.setAlignment(Pos.CENTER);
+        v5.setAlignment(Pos.CENTER);
+
+        v1.setStyle("-fx-background-color : #FF3547;");
+        v2.setStyle("-fx-background-color : #512DA8;");
+        v3.setStyle("-fx-background-color : #48CFAD;");
+        v4.setStyle("-fx-background-color : #02C852;");
+        v5.setStyle("-fx-background-color : #EC407A;");
+
+        return FXCollections.observableArrayList(v1, v2, v3, v4, v5);
+    }
+}
