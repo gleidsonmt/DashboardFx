018bf9a8f7a70db38b8cd60441404d1cb6d494f6
Gleidson <gleidisonmt@gmail.com>
2018-11-28 14:21:56 -0200

    add binding rating

---
+++ src/com/gn/module/profile/Profile.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) Gleidson Neves da Silveira
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.gn.module.profile;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import org.controlsfx.control.Rating;
+
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/**
+ * @author Gleidson Neves da Silveira | gleidisonmt@gmail.com
+ * Create on  28/11/2018
+ */
+public class Profile implements Initializable {
+
+    @FXML private Label note;
+    @FXML private Rating rating;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        note.textProperty().bind(rating.ratingProperty().asString(Locale.ENGLISH, "%.2f"));
+    }
+}
