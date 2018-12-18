9e4bdc538d03e4ae684f34ae800db3f6ac5133c3
Gleidson <gleidisonmt@gmail.com>
2018-12-18 15:34:27 -0200

    update section

--- src/com/gn/control/Section.java
+++ src/com/gn/control/Section.java
@@ -16,6 +16,11 @@
  */
 package com.gn.control;
 
+import animatefx.animation.Pulse;
+import com.jfoenix.controls.JFXButton;
+import jfxtras.labs.scene.control.CalendarLabel;
+import jfxtras.scene.control.CalendarPicker;
+
 /**
  * @author Gleidson Neves da Silveira | gleidisonmt@gmail.com
  * Create on  27/11/2018
@@ -46,6 +51,7 @@ public class Section {
     }
 
     public void setLogged(boolean logged) {
+        new CalendarPicker();
         this.logged = logged;
     }
 
