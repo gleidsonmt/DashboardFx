149e9afab3f036140ed574b5f1492f9fde1cfbd0
Gleidson <gleidisonmt@gmail.com>
2023-06-21 23:24:08 -0300

    add menu buttons

---
+++ src/main/java/io/github/gleidsonmt/dashboardfx/views/controls/MenuButtonPresCreator.java
@@ -0,0 +1,83 @@
+/*
+ *
+ *    Copyright (C) Gleidson Neves da Silveira
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package io.github.gleidsonmt.dashboardfx.views.controls;
+
+import io.github.gleidsonmt.dashboardfx.core.Context;
+import io.github.gleidsonmt.dashboardfx.core.view.layout.creators.TutorialCreator;
+import javafx.geometry.Side;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Gleidson Neves da Silveira | gleidisonmt@gmail.com
+ * Create on  24/05/2023
+ */
+public class MenuButtonPresCreator extends TutorialCreator {
+    public MenuButtonPresCreator(Context context) {
+        super(context);
+        this
+                .title("MenuButton")
+                .text("""
+                        MenuButtonPresCreator is a button which, when clicked or pressed, will show a ContextMenu. A MenuButtonPresCreator shares a very similar API to the Menu control, insofar that you set the items that should be shown in the items ObservableList, and there is a text property to specify the label shown within the MenuButtonPresCreator.
+                        """)
+                .demonstration(createMenuItems(), "")
+                .title("SplitMenuButton")
+                .text("The SplitMenuButton, like the MenuButtonPresCreator is closely associated with the concept of selecting a MenuItem from a menu. Unlike MenuButtonPresCreator, the SplitMenuButton is broken into two pieces, the \"action\" area and the \"menu open\" area.")
+                .demonstration(createSplitMenuItems(), "")
+                .title("Links")
+                .footer(
+                        createDefaultAuthor("MenuButtonPresCreator"),
+                        createDefaultAuthor("SplitMenuButton")
+                )
+                .build();
+    }
+
+    private void createMenuItems(MenuButton menuButton) {
+        for (int i = 0; i < 3; i++) {
+            menuButton.getItems().add(new MenuItem(i + " Item"));
+        }
+    }
+
+    private List<Node> createMenuItems() {
+        List<Node> list = new ArrayList<>();
+        for (Side side : Side.values()) {
+            MenuButton menuButton = new MenuButton();
+            menuButton.setText(side.name());
+            list.add(menuButton);
+            menuButton.setPopupSide(side);
+            createMenuItems(menuButton);
+        }
+        return list;
+    }
+    private List<Node> createSplitMenuItems() {
+        List<Node> list = new ArrayList<>();
+        for (Side side : Side.values()) {
+            SplitMenuButton menuButton = new SplitMenuButton();
+            menuButton.setText(side.name());
+            list.add(menuButton);
+            menuButton.setPopupSide(side);
+            createMenuItems(menuButton);
+        }
+        return list;
+    }
+}
