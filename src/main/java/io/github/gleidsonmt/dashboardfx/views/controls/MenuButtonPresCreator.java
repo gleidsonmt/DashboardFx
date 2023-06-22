6efd2abf1e5e35e070f8375a1111f1f03774ee3f
Gleidson <gleidisonmt@gmail.com>
2023-06-21 23:38:17 -0300

    add menu buttons

--- src/main/java/io/github/gleidsonmt/dashboardfx/views/controls/MenuButtonPresCreator.java
+++ src/main/java/io/github/gleidsonmt/dashboardfx/views/controls/MenuButtonPresCreator.java
@@ -40,10 +40,49 @@ public class MenuButtonPresCreator extends TutorialCreator {
                 .text("""
                         MenuButtonPresCreator is a button which, when clicked or pressed, will show a ContextMenu. A MenuButtonPresCreator shares a very similar API to the Menu control, insofar that you set the items that should be shown in the items ObservableList, and there is a text property to specify the label shown within the MenuButtonPresCreator.
                         """)
-                .demonstration(createMenuItems(), "")
+                .demonstration(createMenuItems(), """
+                        // Constructor
+                        MenuButton menuButton = new MenuButton();
+                        menuButton.setText("MenuButton");
+                        // Set items
+                        menuButton.getItems().add(new MenuItem("Item"));
+                        """, """
+                        <MenuButton text="MenuButton">
+                          <items>
+                            <MenuItem text="Action 1" />
+                            <MenuItem text="Action 2" />
+                          </items>
+                        </MenuButton>
+                        """, """
+                        .menu-button .label .text {
+                        }
+                        
+                        .menu-button .arrow-button .arrow {
+                        }
+                        
+                        """)
                 .title("SplitMenuButton")
                 .text("The SplitMenuButton, like the MenuButtonPresCreator is closely associated with the concept of selecting a MenuItem from a menu. Unlike MenuButtonPresCreator, the SplitMenuButton is broken into two pieces, the \"action\" area and the \"menu open\" area.")
-                .demonstration(createSplitMenuItems(), "")
+                .demonstration(createSplitMenuItems(), """
+                        // Constructor
+                        SplitMenuButton menuButton = new SplitMenuButton();
+                        menuButton.setText("MenuButton");
+                        // Set items
+                        menuButton.getItems().add(new MenuItem("Item"));
+                        """, """
+                        <SplitMenuButton text="SplitMenuButton">
+                          <items>
+                            <MenuItem text="Action 1" />
+                            <MenuItem text="Action 2" />
+                          </items>
+                        </SplitMenuButton>
+                        """, """
+                        .split-menu-button .label .text {
+                        }
+                        
+                        .split-menu-button .arrow-button .arrow {
+                        }
+                        """)
                 .title("Links")
                 .footer(
                         createDefaultAuthor("MenuButtonPresCreator"),
