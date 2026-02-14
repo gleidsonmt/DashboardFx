e8a5b249ec723ecfb6d611ba20d07d973d23aa97
Gleidson Neves da Silveira <gleidisonmt@gmail.com>
2026-02-14 17:30:17 -0300

    revising root

--- src/main/java/io/github/gleidsonmt/dashboardfx/dashboard/NavUserSection.java
+++ src/main/java/io/github/gleidsonmt/dashboardfx/dashboard/NavUserSection.java
@@ -124,7 +124,7 @@ public class NavUserSection extends HBox {
         MenuItem menuManageAccount = new MenuItem("Manage Account");
         menuManageAccount.setOnAction(_ -> {
             Root root = (Root) this.getScene().getRoot();
-            root.getLayout().setCurrentModule(new ProfilePage());
+            root.getLayout().setModule(new ProfilePage());
         });
         menuManageAccount.getStyleClass().add("menu-item-last");
         menuManageAccount.setGraphic(new SVGIcon(Icon.MANAGE_ACCOUNTS));
